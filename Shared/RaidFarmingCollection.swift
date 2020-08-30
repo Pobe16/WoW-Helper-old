789ef494e68f4d0fa6ba94c9dcb0933ac2f5c7b5
Mikolaj Lukasik <mlukasik@gmail.com>
2020-08-31 00:41:59 +0100

    decoded user encounters

--- Shared/RaidFarmingCollection.swift
+++ Shared/RaidFarmingCollection.swift
@@ -11,7 +11,11 @@ struct RaidFarmingCollection: View {
     @EnvironmentObject var authorization: Authentication
     @EnvironmentObject var gameData: GameData
     
-    @State var characterEncounters: [Any] = []
+    @State var errorDescription: String?
+    
+    @State var dataCreationDate: String = "Loading"
+    
+    @State var characterEncounters: CharacterRaidEncounters?
     
     let character: CharacterInProfile
     let data = (1...10).map { CGFloat($0) }
@@ -26,14 +30,15 @@ struct RaidFarmingCollection: View {
                 
                 Section(header:
                             HStack{
-                                Spacer()
+//                                Spacer()
                                 Text("Section 1")
                                     .font(.title)
+                                    .padding()
+                                    .padding(.leading, 15)
                                 Spacer()
                             }
-                            .padding(.vertical)
-                            .padding(.horizontal, 0)
                             .background(Color.gray.opacity(0.7))
+                            .cornerRadius(30)
                 ) {
                     ForEach(data, id: \.self) { _ in
                         Color(
@@ -53,10 +58,104 @@ struct RaidFarmingCollection: View {
         .onAppear {
             downloadRaidEncounters()
         }
+        .toolbar {
+            ToolbarItem(placement: .status) {
+                HStack{
+                    Spacer()
+                    Text("Last refreshed: \(dataCreationDate)")
+                }
+            }
+        }
+    }
+    
+    func setDataCreationDate(to date: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+//        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        dateFormatter.locale = Locale.current
+        let dateString = dateFormatter.string(from: date)
+        dataCreationDate = dateString
     }
     
     func downloadRaidEncounters() {
+        let requestUrlAPIHost = UserDefaults.standard.object(forKey: "APIRegionHost") as? String ?? APIRegionHostList.Europe
+        let requestUrlAPIFragment =
+            "/profile/wow/character"    + "/" +
+            character.realm.slug        + "/" +
+            character.name.lowercased() + "/" +
+            "encounters/raids"
+            
+        let strippedAPIUrl = requestUrlAPIHost + requestUrlAPIFragment
         
+        if let savedData = JSONCoreDataManager.shared.fetchJSONData(withName: strippedAPIUrl, maximumAgeInDays: 0.0416) {
+            
+            setDataCreationDate(to: savedData.creationDate!)
+            
+            decodeEncountersData(savedData.data!)
+            
+            return
+        }
+        
+        let regionShortCode = APIRegionShort.Code[UserDefaults.standard.integer(forKey: "loginRegion")]
+        let requestAPINamespace = "profile-\(regionShortCode)"
+        let requestLocale = UserDefaults.standard.object(forKey: "localeCode") as? String ?? EuropeanLocales.BritishEnglish
+        
+        let fullRequestURL = URL(string:
+                                    requestUrlAPIHost +
+                                    requestUrlAPIFragment +
+                                    "?namespace=\(requestAPINamespace)" +
+                                    "&locale=\(requestLocale)" +
+                                    "&access_token=\(authorization.oauth2?.accessToken ?? "")"
+        )!
+        print(fullRequestURL)
+        
+        guard let req = authorization.oauth2?.request(forURL: fullRequestURL) else { return }
+        
+        let task = authorization.oauth2?.session.dataTask(with: req) { data, response, error in
+            if let response = response as? HTTPURLResponse,
+               response.statusCode == 200,
+               let data = data {
+                
+//                print(data)
+                setDataCreationDate(to: Date())
+                
+                decodeEncountersData(data, fromURL: fullRequestURL)
+
+            } else {
+                if let error = error {
+                    // something went wrong, check the error
+                    print("error")
+                    print(error.localizedDescription)
+                }
+                if let response = response as? HTTPURLResponse {
+                    
+                    print(response.statusString)
+                }
+            }
+        }
+        task?.resume()
+    }
+    
+    func decodeEncountersData(_ data: Data, fromURL url: URL? = nil) {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        
+        do {
+            let dataResponse = try decoder.decode(CharacterRaidEncounters.self, from: data)
+            
+            if let url = url {
+                JSONCoreDataManager.shared.saveJSON(data, withURL: url)
+            }
+            
+            characterEncounters = dataResponse
+                
+            
+            
+        } catch {
+            print(error)
+        }
     }
 }
 
@@ -68,10 +167,14 @@ struct RaidFarmingCollection_Previews: PreviewProvider {
         RaidFarmingCollection(character: placeholders.characterInProfile)
         .previewLayout(.fixed(width: 320, height: 568))
         .previewDisplayName("iPhone SE 1st gen")
-                    
-        RaidFarmingCollection(character: placeholders.characterInProfile)
-        .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
-        .previewDisplayName("iPhone 8")
+//
+//        RaidFarmingCollection(character: placeholders.characterInProfile)
+//        .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+//        .previewDisplayName("iPhone 8")
+//
+//        RaidFarmingCollection(character: placeholders.characterInProfile)
+//        .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+//        .previewDisplayName("iPhone 11 Pro Max")
     }
 }
 #endif
