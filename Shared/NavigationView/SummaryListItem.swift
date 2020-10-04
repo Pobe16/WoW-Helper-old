ee3c7bb19360e9de39c9de99c4b72b40e5caffee
Mikolaj Lukasik <mlukasik@gmail.com>
2020-10-05 00:03:04 +0100

    started working on the summary page
    changed the accent colour to blue / aqua

---
+++ Shared/NavigationView/SummaryListItem.swift
@@ -0,0 +1,29 @@
+//
+//  SummaryListItem.swift
+//  WoWWidget
+//
+//  Created by Mikolaj Lukasik on 04/10/2020.
+//
+
+import SwiftUI
+
+struct SummaryListItem: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "person.2.square.stack")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 63, height: 63)
+                .cornerRadius(15, antialiased: true)
+                .foregroundColor(.accentColor)
+            Text("Summary")
+            
+        }
+    }
+}
+
+struct SummaryListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryListItem()
+    }
+}
