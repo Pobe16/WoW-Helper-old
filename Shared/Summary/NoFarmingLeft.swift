e952be3a7b73face2b000ddd3ec94b3a8bd4e5f2
Mikolaj Lukasik <mlukasik@gmail.com>
2020-10-13 00:37:25 +0100

    finished no farming view
    added the summary preview size selection
    limited the preview to just one size
    properly set animations and their connections in no farming left view

--- Shared/Summary/NoFarmingLeft.swift
+++ Shared/Summary/NoFarmingLeft.swift
@@ -8,119 +8,131 @@
 import SwiftUI
 
 struct NoFarmingLeft: View {
+    @Namespace private var noFarmingLeft
+    
+    let summarySize: summaryPreviewSize
     
     let character: CharacterInProfile
     
     var body: some View {
-        ScrollView(.horizontal) {
-            
-            HStack(alignment: .top, spacing: 20) {
-                
-                VStack{
-                    HStack(alignment: .top) {
-                        CharacterImage(character: character)
-                            .padding()
-                        Spacer(minLength: 0)
-                        Text("\(character.name), \(character.level)")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .whiteTextWithBlackOutlineStyle()
-                            .padding()
-                            .minimumScaleFactor(0.5)
-                    }
+        
+        switch summarySize {
+        case .large:
+            VStack{
+                HStack(alignment: .top) {
+                    CharacterImage(character: character)
+                        .padding()
+                        .matchedGeometryEffect(id: "characterImage", in: noFarmingLeft)
                     Spacer(minLength: 0)
-                    HStack {
-                        Spacer(minLength: 0)
-                        Text("All done!")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                            .whiteTextWithBlackOutlineStyle()
-                            .minimumScaleFactor(0.9)
-                        Spacer(minLength: 0)
-                    }
-                    .padding()
+                    Text("\(character.name), \(character.level)")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .whiteTextWithBlackOutlineStyle()
+                        .padding()
+                        .minimumScaleFactor(0.5)
+                        .matchedGeometryEffect(id: "characterName", in: noFarmingLeft)
                 }
-                .frame(width: 292, height: 311)
-                .background(
-                    Image("Goldshire_Inn")
-                        .resizable()
-                        .scaledToFill()
-                )
-                .cornerRadius(15)
-                               
-                VStack{
-                    HStack(alignment: .top) {
-                        CharacterImage(character: character)
-                            .padding()
-                        Spacer(minLength: 0)
-                        Text("\(character.name), \(character.level)")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .whiteTextWithBlackOutlineStyle()
-                            .padding()
-                            .minimumScaleFactor(0.9)
-//                        Spacer()
-                    }
+                Spacer(minLength: 0)
+                HStack {
                     Spacer(minLength: 0)
-                    HStack {
-                        Spacer(minLength: 0)
-                        Text("All done!")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                            .whiteTextWithBlackOutlineStyle()
-                            .minimumScaleFactor(0.9)
-                        Spacer(minLength: 0)
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom)
-                }
-                .frame(width: 292, height: 141)
-                .background(
-                    Image("Goldshire_Inn")
-                        .resizable()
-                        .scaledToFill()
-                )
-                .cornerRadius(15)
-                
-                VStack{
-                    HStack {
-                        CharacterImage(character: character, frameSize: 50)
-                            .padding()
-                        Spacer()
-                    }
+                    Text("All done!")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .whiteTextWithBlackOutlineStyle()
+                        .minimumScaleFactor(0.9)
+                        .matchedGeometryEffect(id: "allDone", in: noFarmingLeft)
                     Spacer(minLength: 0)
-                    HStack {
-                        Spacer(minLength: 0)
-                        Text("All done!")
-                            .font(.title3)
-                            .fontWeight(.semibold)
-                            .whiteTextWithBlackOutlineStyle()
-                            .padding()
-                            .minimumScaleFactor(0.5)
-                        Spacer(minLength: 0)
-                    }
                 }
-                .frame(width: 141, height: 141)
-                .background(
-                    Image("Goldshire_Inn_Mini")
-                        .resizable()
-                        .scaledToFill()
-                )
-                .cornerRadius(15)
+                .padding()
             }
-            .padding()
+            .frame(width: 292, height: 311)
+            .background(
+                Image("Goldshire_Inn")
+                    .resizable()
+                    .scaledToFill()
+                    .matchedGeometryEffect(id: "background", in: noFarmingLeft)
+            )
+            .cornerRadius(15)
+            
+        case .medium:
+            VStack{
+                HStack(alignment: .top) {
+                    CharacterImage(character: character)
+                        .padding()
+                        .matchedGeometryEffect(id: "characterImage", in: noFarmingLeft)
+                    Spacer(minLength: 0)
+                    Text("\(character.name), \(character.level)")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .whiteTextWithBlackOutlineStyle()
+                        .padding()
+                        .minimumScaleFactor(0.9)
+                        .matchedGeometryEffect(id: "characterName", in: noFarmingLeft)
+                }
+                Spacer(minLength: 0)
+                HStack {
+                    Spacer(minLength: 0)
+                    Text("All done!")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .whiteTextWithBlackOutlineStyle()
+                        .minimumScaleFactor(0.9)
+                        .matchedGeometryEffect(id: "allDone", in: noFarmingLeft)
+                    Spacer(minLength: 0)
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+            }
+            .frame(width: 292, height: 141)
+            .background(
+                Image("Goldshire_Inn")
+                    .resizable()
+                    .scaledToFill()
+                    .matchedGeometryEffect(id: "background", in: noFarmingLeft)
+            )
+            .cornerRadius(15)
+            
+        case .small:
+            VStack{
+                HStack {
+                    CharacterImage(character: character, frameSize: 50)
+                        .padding()
+                        .matchedGeometryEffect(id: "characterImage", in: noFarmingLeft)
+                    Spacer()
+                }
+                Spacer(minLength: 0)
+                HStack {
+                    Spacer(minLength: 0)
+                    Text("All done!")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .whiteTextWithBlackOutlineStyle()
+                        .padding()
+                        .minimumScaleFactor(0.5)
+                        .matchedGeometryEffect(id: "allDone", in: noFarmingLeft)
+                    Spacer(minLength: 0)
+                }
+            }
+            .frame(width: 141, height: 141)
+            .background(
+                Image("Goldshire_Inn_Mini")
+                    .resizable()
+                    .scaledToFill()
+                    .matchedGeometryEffect(id: "background", in: noFarmingLeft)
+            )
+            .cornerRadius(15)
         }
     }
 }
 
 struct NoFarmingLeft_Previews: PreviewProvider {
     static var previews: some View {
-        NoFarmingLeft(character: placeholders.characterInProfile)
+        NoFarmingLeft(summarySize: .large, character: placeholders.characterInProfile)
             .preferredColorScheme(.dark)
             .previewLayout(.fixed(width: 850, height: 350))
             .environmentObject(Authentication())
         
-        NoFarmingLeft(character: placeholders.characterInProfile)
+        NoFarmingLeft(summarySize: .large, character: placeholders.characterInProfile)
             .previewLayout(.fixed(width: 850, height: 350))
             .environmentObject(Authentication())
     }
--- Shared/Summary/SummaryMain.swift
+++ Shared/Summary/SummaryMain.swift
@@ -7,13 +7,29 @@
 
 import SwiftUI
 
+enum summaryPreviewSize: String {
+    case large  = "Large"
+    case medium = "Medium"
+    case small  = "Small"
+}
+
 struct SummaryMain: View {
     @EnvironmentObject var gameData: GameData
+    
+    @State var summarySize: summaryPreviewSize = .large
+    
+    #if os(iOS)
+    let raidSettingPlacement: ToolbarItemPlacement = .primaryAction
+    #elseif os(macOS)
+    let raidSettingPlacement: ToolbarItemPlacement = .confirmationAction
+    #endif
+    
     var body: some View {
         if gameData.loadingAllowed {
             ScrollView {
                 ForEach(gameData.characterRaidEncounters, id: \.character.id) { GDCharacterEncounters in
                     SingleCharacterSummary(
+                        summarySize: summarySize,
                         character: getCharacterBasedOn(encounters: GDCharacterEncounters),
                         characterEncounters: GDCharacterEncounters
                     )
@@ -26,7 +42,24 @@ struct SummaryMain: View {
                     Text("Farming opportunities:")
                         .font(.title2)
                 }
+                ToolbarItem(placement: raidSettingPlacement) {
+                    Menu {
+                        Picker(selection: $summarySize.animation(.linear(duration: 0.2)), label: Text("")) {
+                            Text(summaryPreviewSize.large.rawValue).tag(summaryPreviewSize.large)
+                            Text(summaryPreviewSize.medium.rawValue).tag(summaryPreviewSize.medium)
+                            Text(summaryPreviewSize.small.rawValue).tag(summaryPreviewSize.small)
+                        }
+                    }
+                    label: {
+                        Label("Preview Size", systemImage: "gear")
+                            .font(.title3)
+                    }
+                }
             })
+            .onAppear( perform: { loadOptionsSelection() } )
+            .onChange(of: summarySize) { (value) in
+                saveOptionsSelection(value)
+            }
         } else {
             DataLoadingInfo()
                 .toolbar(content: {
@@ -44,6 +77,15 @@ struct SummaryMain: View {
             GDCharacter.name == encounters.character.name && GDCharacter.realm.name == encounters.character.realm.name
         }
         return character!
+    }
+    
+    func loadOptionsSelection() {
+        let option = UserDefaults.standard.object(forKey: UserDefaultsKeys.summaryPreviewSize) as? String ?? summaryPreviewSize.large.rawValue
+        summarySize = summaryPreviewSize(rawValue: option) ?? summaryPreviewSize.large
+    }
+    
+    func saveOptionsSelection(_ value: summaryPreviewSize) {
+        UserDefaults.standard.setValue(value.rawValue, forKey: UserDefaultsKeys.summaryPreviewSize)
     }
 }
 
